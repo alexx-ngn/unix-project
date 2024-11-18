18b817558dbf115b6656136805407eebad57a724
alex <49459824+alexx-ngn@users.noreply.github.com>
2024-11-17 22:09:14 -0500

    Update gen.sh
    Fixed output of iso to point towards the out directory

--- gen.sh
+++ gen.sh
@@ -20,7 +20,7 @@ find -follow -type f -print0 | xargs --null md5sum > md5sum.txt
 
 cd $OUT
 
-xorriso -as mkisofs -o $ISO -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
+xorriso -as mkisofs -o "$OUT/$(basename $1)" -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
 -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 \
 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot \
 -isohybrid-gpt-basdat $WD
