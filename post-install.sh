3ca9669cb7d290f4e5b3c9e5e06e48e7354d7210
Alexander Nguyen <2381040@edu.vaniercollege.qc.ca>
2024-12-01 01:04:22 -0500

    added docker compose containers

--- post-install.sh
+++ post-install.sh
@@ -4,6 +4,6 @@ git clone https://github.com/alexx-ngn/unix-project /home/alex/unix-project
 mkdir /home/alex/backups
 chown -R alex:alex /home/alex
 ansible-playbook /home/alex/unix-project/ansible/playbook.yml
-cp /home/alex/unix-project/backup.server /etc/systemd/system/
+cp /home/alex/unix-project/backup.service /etc/systemd/system/
 cp /home/alex/unix-project/backup.timer /etc/systemd/system/
 systemctl enable --now backup.timer
