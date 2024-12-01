2ff51763270bc8e059da554c7633d1c3325e8d92
Alexander Nguyen <2381040@edu.vaniercollege.qc.ca>
2024-12-01 11:33:31 -0500

    modified docker role to start containers

--- post-install.sh
+++ post-install.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/alexx-ngn/unix-project /home/alex/unix-project
 mkdir /home/alex/backups
 chown -R alex:alex /home/alex
+ansible-galaxy collection install community.docker
 ansible-playbook /home/alex/unix-project/ansible/playbook.yml
 cp /home/alex/unix-project/backup.service /etc/systemd/system/
 cp /home/alex/unix-project/backup.timer /etc/systemd/system/
