4d9f252ade7206d01275abd2cd9be293c8ed4cd7
Alexander Nguyen <2381040@edu.vaniercollege.qc.ca>
2024-12-01 00:48:48 -0500

    modified post-install

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/alexx-ngn/unix-project /root/unix-project
-ansible-playbook /root/unix-project/ansible/playbook.yml
+git clone https://github.com/alexx-ngn/unix-project /home/alex/unix-project
+mkdir /home/alex/backups
+chown -R alex:alex /home/alex
+ansible-playbook /home/alex/unix-project/ansible/playbook.yml
+cp /home/alex/unix-project/backup.server /etc/systemd/system/
+cp /home/alex/unix-project/backup.timer /etc/systemd/system/
+systemctl enable --now backup.timer
